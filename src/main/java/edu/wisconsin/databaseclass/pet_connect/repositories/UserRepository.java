8fa241065fe2be399fa7a3640660f36c0ea5d2a3
brileighx <kirchgessner@wisc.edu>
2024-07-02 00:15:48 -0400

    Initial project structure setup

---
+++ src/main/java/edu/wisconsin/databaseclass/pet_connect/repositories/UserRepository.java
@@ -0,0 +1,5 @@
+package edu.wisconsin.databaseclass.pet_connect.repositories;
+
+public interface UserRepository { //Make extend JPA (add dependency to pom.xml)
+//Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/edu/wisconsin/databaseclass/pet_connect/services/UserService.java
@@ -0,0 +1,8 @@
+package edu.wisconsin.databaseclass.pet_connect.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    //Service Methods
+}
