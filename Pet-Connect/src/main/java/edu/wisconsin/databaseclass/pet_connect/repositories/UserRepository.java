0dccb239dad2d91fc095d6e9bbca297e06a7b9c5
BriLeighk <kirchgessner@wisc.edu>
2024-07-19 15:23:45 -0400

    Removed nested project directory

--- Pet-Connect/src/main/java/edu/wisconsin/databaseclass/pet_connect/repositories/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package edu.wisconsin.databaseclass.pet_connect.repositories;
-
-public interface UserRepository { //Make extend JPA (add dependency to pom.xml)
-//Optional<User> findByUsername(String username);
-}
--- Pet-Connect/src/main/java/edu/wisconsin/databaseclass/pet_connect/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package edu.wisconsin.databaseclass.pet_connect.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-    //Service Methods
-}
