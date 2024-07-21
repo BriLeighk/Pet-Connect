b1f167980a886151eee3d358ebd36cf43031a6ce
BriLeighk <kirchgessner@wisc.edu>
2024-07-21 05:40:33 -0400

    implemented user create account + login feature

--- src/main/java/edu/wisconsin/databaseclass/pet_connect/services/UserService.java
+++ src/main/java/edu/wisconsin/databaseclass/pet_connect/services/UserService.java
@@ -1,34 +1,47 @@
 package edu.wisconsin.databaseclass.pet_connect.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import edu.wisconsin.databaseclass.pet_connect.entities.User;
 import edu.wisconsin.databaseclass.pet_connect.repositories.UserRepository;
 
-import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class UserService {
+    private static final Logger logger = Logger.getLogger(UserService.class.getName());
 
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
+    // Used my Message and Favorites to get user records from MySQL database via userId
     public User getUserById(int userId) {
         return userRepository.findById(userId).orElse(null);
     }
 
-    public User saveUser(User user) {
+    // saves user record to the MySQL database
+    public User saveUser(User user, String rawPassword) {
+        user.setPassword(passwordEncoder.encode(rawPassword)); // hash password and store in User table
         return userRepository.save(user);
     }
 
     public void deleteUser(int userId) {
         userRepository.deleteById(userId);
     }
+
+    public User findByEmail(String email) {
+        logger.info("Finding user by email: " + email);
+        User user = userRepository.findByEmail(email);
+        if (user != null) {
+            logger.info("User found: " + user.getEmail());
+        } else {
+            logger.info("User not found with email: " + email);
+        }
+        return user;
+    }
 }
-
-
